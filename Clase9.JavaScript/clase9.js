987045144cf559e062d886beb215bd60ce32b6f5
LeaTraficante <leatraficante@gmail.com>
2025-04-16 16:08:19 +0200

    cambios en clase 9

--- Clase9.JavaScript/clase9.js
+++ Clase9.JavaScript/clase9.js
@@ -1,97 +1,109 @@
-let miNombre;
-let miEdad;
-console.log(miEdad, miNombre)
+// let miNombre;
+// let miEdad;
+// console.log(miEdad, miNombre)
 
-miNombre = "Leandro";
-miEdad = 36;
+// miNombre = "Leandro";
+// miEdad = 36;
 
-const miApellido = "Traficante";
-// miApellido = "Traficante"; // esto tira error porque una const no se puede cambiar el valor ya asigando
+// const miApellido = "Traficante";
+// // miApellido = "Traficante"; // esto tira error porque una const no se puede cambiar el valor ya asigando
 
-console.log(miNombre, miEdad, miApellido)
+// console.log(miNombre, miEdad, miApellido)
 
-let x = 8;
-let y = 8;
+// let x = 8;
+// let y = 8;
 
-if (x < y) {
-    console.log("X es mayor a Y")
-} else if (x == y) {
-    console.log("X == Y")
-} else {
-    console.log("X no es mayor a Y")
-};
-
-
-// SWITCH
-let diaDeLaSemana = parseInt(prompt("Ingrese el numero de la semana"));
-let win = false;
-
-switch (diaDeLaSemana) {
-    case 1:
-        console.log("Lunes");
-        break;
-    case 2:
-        console.log("Martes");
-        break;
-    case 3:
-        console.log("Miercoles");
-        break;
-    case 4:
-        console.log("Jueves");
-        win = true;
-        break;
-    case 5:
-        console.log("Viernes");
-        break;
-    case 6:
-        console.log("Sabado");
-        break;
-    case 7:
-        console.log("Domingo");
-        break;
-    default:
-        console.log("Seleccione un numero valido")
-        break;
-};
-
-if (win) {
-    alert("Es el día ganador!")
-};
-
-let i = 8;
-let k = 7;
-
-// while (i <= k) { // primero controla si esta condicion se cumple
-//     console.log(i); // luego se ejecuta este codigo
-//     i++; 
+// if (x < y) {
+//     console.log("X es mayor a Y")
+// } else if (x == y) {
+//     console.log("X == Y")
+// } else {
+//     console.log("X no es mayor a Y")
 // };
 
-do {
-    console.log(i); // primero se ejecuta este codigo
-    i++; 
-} while (i <= k); // luego ve si se cumple la condicion
+
+// // SWITCH
+// let diaDeLaSemana = parseInt(prompt("Ingrese el numero de la semana"));
+// let win = false;
+
+// switch (diaDeLaSemana) {
+//     case 1:
+//         console.log("Lunes");
+//         break;
+//     case 2:
+//         console.log("Martes");
+//         break;
+//     case 3:
+//         console.log("Miercoles");
+//         break;
+//     case 4:
+//         console.log("Jueves");
+//         win = true;
+//         break;
+//     case 5:
+//         console.log("Viernes");
+//         break;
+//     case 6:
+//         console.log("Sabado");
+//         break;
+//     case 7:
+//         console.log("Domingo");
+//         break;
+//     default:
+//         console.log("Seleccione un numero valido")
+//         break;
+// };
+
+// if (win) {
+//     alert("Es el día ganador!")
+// };
+
+// let i = 8;
+// let k = 7;
+
+// // while (i <= k) { // primero controla si esta condicion se cumple
+// //     console.log(i); // luego se ejecuta este codigo
+// //     i++; 
+// // };
+
+// do {
+//     console.log(i); // primero se ejecuta este codigo
+//     i++; 
+// } while (i <= k); // luego ve si se cumple la condicion
 
 
-let m;
+// let m;
 
-for (let m = 1; m <= 8; m++) {
-    console.log("Bucle vuelta numero: ", m);
-};
+// for (let m = 1; m <= 8; m++) {
+//     console.log("Bucle vuelta numero: ", m);
+// };
 
-const persona = {
-    name: "Leandro",
-    edad: 37,
-    email: "leandro@leandro.com"
-};
+// const persona = {
+//     name: "Leandro",
+//     edad: 37,
+//     email: "leandro@leandro.com"
+// };
 
-console.log(persona)
+// console.log(persona)
 
-for (let i in persona) {
-    console.log(i) // devuleve los keys, no los valores
-};
+// for (let i in persona) {
+//     console.log(i) // devuleve los keys, no los valores
+// };
 
-let colores = ["rojo", "azul", "verde"]
+// let colores = ["rojo", "azul", "verde"]
 
-for (let color of colores) {
-    console.log(color)
-};
+// for (let color of colores) {
+//     console.log(color)
+// };
+
+
+let array1 = [];
+let array2 = [...array1];
+
+
+console.log(array1 === array2); // por que false?
+
+array1.push(10);
+console.log(array1)
+console.log(array2)
+console.log(array1 === array2);

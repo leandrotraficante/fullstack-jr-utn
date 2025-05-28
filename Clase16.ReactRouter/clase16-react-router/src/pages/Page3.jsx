36d1d738858ed508135ac78a4d80d77fa298357d
leandrotraficante <leandrotraficante@hotmail.com>
2025-05-28 15:51:33 +0200

    correcion try/catch

--- Clase16.ReactRouter/clase16-react-router/src/pages/Page3.jsx
+++ Clase16.ReactRouter/clase16-react-router/src/pages/Page3.jsx
@@ -1,37 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-
 function Page3() {
-    const dogApi = "https://dog.ceo/api/breeds/image/random";
-    const [dog, setDog] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
+  const dogApi = "https://dog.ceo/api/breeds/image/random";
+  const [dog, setDog] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-    // La buena practica es primero crear la funcion async, y luego llamarla dentro del useEffect:
-    const dogAsync = async () => {
-        const response = await axios.get(dogApi)
-        setDog(response.data)
-        setIsLoading(false)
-    };
+  const dogAsync = async () => {
+    try {
+      const response = await axios.get(dogApi);
+      setDog(response.data);
+    } catch (error) {
+      console.log("Error fetching dog image:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    useEffect(() => {
-        try {
-            dogAsync();
-        } catch (error) {
-            console.log(error)
-        }
-    }, [])
+  useEffect(() => {
+    dogAsync();
+  }, []);
 
-    return (
-        <div style={{ backgroundColor: 'lightgrey', height: 200 }}>
-            <h3>EJEMPLO DE CONSUMIR API CON AXIOS + ASYNC AWAIT</h3>
-            {isLoading ?
-                <h2>Loading...</h2>
-                :
-                <img src={dog.message} alt="" height={150} />
-            }
-        </div>
-    );
+  return (
+    <div style={{ backgroundColor: 'lightgrey', height: 200 }}>
+      <h3>EJEMPLO DE CONSUMIR API CON AXIOS + ASYNC AWAIT</h3>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <img src={dog.message} alt="Dog" height={150} />
+      )}
+    </div>
+  );
 }
 
 export default Page3;

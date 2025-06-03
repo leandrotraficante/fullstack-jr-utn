28c6f58fd69c53ab122dafc002f629ee7184ea73
leandrotraficante <leandrotraficante@hotmail.com>
2025-06-03 19:14:59 +0200

    dev branch, ejercicios ReactJS

---
+++ Ejercicios/ReactJS/TP.Entregable.ReactJS.01.LeandroTraficante/calculadora-react/src/components/Calcular.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Calcular() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Calcular;
---
+++ Ejercicios/ReactJS/TP.Entregable.ReactJS.01.LeandroTraficante/calculadora-react/src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Content() {
+    return (
+        <div>
+            "CONTENT"
+        </div>
+    );
+}
+
+export default Content;
